4e509ac36d5f47eefae92fe1ee35d9ef3c747f8c
Steffen Hirte <shirte@users.noreply.github.com>
2024-11-25 02:42:53 +0100

    Move request steps to client steps

--- tests/steps/client.py
+++ tests/steps/client.py
@@ -1,6 +1,12 @@
+import asyncio
+import json
+
 import pytest_asyncio
 from asgi_lifespan import LifespanManager
 from fastapi.testclient import TestClient
+from pytest_bdd import parsers, then, when
+
+from .async_step import async_step
 
 
 @pytest_asyncio.fixture
@@ -10,3 +16,37 @@ async def client():
     async with LifespanManager(app):
         client = TestClient(app)
         yield client
+
+
+@when(
+    parsers.parse("the client sends a POST request to {url} with content\n{data}"),
+    target_fixture="response",
+)
+def post_request(client, url, data):
+    response = client.post(url, json=json.loads(data))
+    print(response.json())
+    return response
+
+
+@when(parsers.parse("the client requests {url}"), target_fixture="response")
+def response(client, url):
+    response = client.get(url)
+    return response
+
+
+@then(parsers.parse("the status code of the response is {expected_status_code:d}"))
+def check_status_code(response, expected_status_code):
+    status_code = response.status_code
+    assert status_code == expected_status_code
+
+
+@then(parsers.parse("the client receives {expected_response}"))
+def check_response(response, expected_response):
+    decoded = json.loads(expected_response)
+    assert response.json() == decoded, f"Expected {decoded}, got {response.json()}"
+
+
+@when(parsers.parse("we wait for {seconds:d} seconds"))
+@async_step
+async def wait_for_seconds(seconds):
+    await asyncio.sleep(seconds)
