62dad3ea4628da5a50a38afa0b902d625f523117
Steffen Hirte <shirte@users.noreply.github.com>
2024-11-26 08:32:24 +0100

    Integrate hydra config in fast api

--- nerdd_backend/lifespan/initialize_app_lifespan.py
+++ nerdd_backend/lifespan/initialize_app_lifespan.py
@@ -1,25 +1,40 @@
 from nerdd_link import KafkaChannel
+from nerdd_link.tests import DummyChannel
+from omegaconf import DictConfig
 
-from ..data import RethinkDbRepository
-from ..settings import KAFKA_BROKER_URL
+from ..data import MemoryRepository, RethinkDbRepository
 from .abstract_lifespan import AbstractLifespan
 
 
 class InitializeAppLifespan(AbstractLifespan):
-    def __init__(self):
+    def __init__(self, config: DictConfig):
         super().__init__()
+        self.config = config
 
     async def start(self, app):
-        channel = self.get_channel()
-        repository = self.get_repository()
+        channel = self.get_channel(self.config)
+        repository = self.get_repository(self.config)
 
         app.state.repository = repository
         app.state.channel = channel
+        app.state.config = self.config
 
         await repository.initialize()
 
-    def get_channel(self):
-        return KafkaChannel(KAFKA_BROKER_URL)
+    def get_channel(self, config: DictConfig):
+        if config.channel.name == "kafka":
+            return KafkaChannel(config.channel.broker_url)
+        elif config.channel.name == "memory":
+            return DummyChannel()
+        else:
+            raise ValueError(f"Unsupported channel name: {config.channel.name}")
 
-    def get_repository(self):
-        return RethinkDbRepository()
+    def get_repository(self, config: DictConfig):
+        if config.db.name == "rethinkdb":
+            return RethinkDbRepository(
+                config.db.host, config.db.port, config.db.database_name
+            )
+        elif config.db.name == "memory":
+            return MemoryRepository()
+        else:
+            raise ValueError(f"Unsupported database: {config.db.name}")
