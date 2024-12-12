05a3431e6a6e9f84c7dc16a5dbc92f49cd8d55a5
Steffen Hirte <shirte@users.noreply.github.com>
2024-12-12 09:07:21 +0100

    Adapt sources router

--- nerdd_backend/routers/sources.py
+++ nerdd_backend/routers/sources.py
@@ -10,9 +10,76 @@ from fastapi import APIRouter, HTTPException, Request, UploadFile
 from fastapi.encoders import jsonable_encoder
 
 from ..data import RecordNotFoundError, Repository
-from ..models import Source
+from ..models import Source, SourcePublic
+
+__all__ = ["sources_router", "put_multiple_sources"]
 
 sources_router = APIRouter(prefix="/sources")
+
+
+@sources_router.put("", include_in_schema=False)
+@sources_router.put("/")
+async def put_source(file: UploadFile, format: Optional[str] = None, request: Request = None):
+    app = request.app
+    repository: Repository = app.state.repository
+    media_root = app.state.config.media_root
+
+    # create uuid
+    uuid = uuid4()
+
+    # create path to new file
+    # TODO: use FileSystem
+    path = os.path.join(media_root, "sources", str(uuid))
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    # store file
+    async with aiofiles.open(path, "wb") as out_file:
+        while content := await file.read(1024):  # async read chunk
+            await out_file.write(content)  # async write chunk
+
+    # create media object
+    source = Source(
+        id=str(uuid),
+        format=format,
+        filename=file.filename,
+    )
+    await repository.upsert_source(source)
+
+    return SourcePublic(**source.model_dump())
+
+
+@sources_router.get("/{uuid}")
+async def get_source(uuid: str, request: Request):
+    app = request.app
+    repository: Repository = app.state.repository
+    try:
+        source = await repository.get_source_by_id(uuid)
+    except RecordNotFoundError as e:
+        raise HTTPException(status_code=404, detail="Source not found") from e
+
+    return SourcePublic(**source.model_dump())
+
+
+@sources_router.delete("/{uuid}")
+async def delete_source(uuid: str, request: Request):
+    app = request.app
+    repository: Repository = app.state.repository
+    media_root = app.state.config.media_root
+
+    try:
+        await repository.get_source_by_id(uuid)
+    except RecordNotFoundError as e:
+        raise HTTPException(status_code=404, detail="Source not found") from e
+
+    # delete file from disk
+    # TODO: use FileSystem
+    path = os.path.join(media_root, "sources", str(uuid))
+    os.remove(path)
+
+    # delete source from database
+    await repository.delete_source_by_id(uuid)
+
+    return {"message": "Source deleted successfully"}
 
 
 async def put_multiple_sources(
@@ -32,7 +99,7 @@ async def put_multiple_sources(
         async def _put_input(input: str):
             file_stream = BytesIO(input.encode("utf-8"))
             file = UploadFile(file_stream)
-            return await put_source(request, file=file)
+            return await put_source(file=file, request=request)
 
         sources_from_inputs = await asyncio.gather(*[_put_input(input) for input in inputs])
         all_sources += sources_from_inputs
@@ -51,67 +118,6 @@ async def put_multiple_sources(
     # create a merged file with all sources
     file_stream = BytesIO(json.dumps(jsonable_encoder(all_sources_objects)).encode("utf-8"))
     file = UploadFile(file_stream, filename="input.json")
-    result_source = await put_source(request, file=file, format="json")
+    result_source = await put_source(file=file, format="json", request=request)
 
     return result_source
-
-
-@sources_router.put("", include_in_schema=False)
-@sources_router.put("/")
-async def put_source(request: Request, file: UploadFile, format: Optional[str] = None):
-    app = request.app
-    repository: Repository = app.state.repository
-    media_root = app.state.config.media_root
-
-    # create uuid
-    uuid = uuid4()
-
-    # create path to new file
-    path = os.path.join(media_root, "sources", str(uuid))
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-
-    # store file
-    async with aiofiles.open(path, "wb") as out_file:
-        while content := await file.read(1024):  # async read chunk
-            await out_file.write(content)  # async write chunk
-
-    # create media object
-    source = Source(
-        id=str(uuid),
-        format=format,
-        filename=file.filename,
-    )
-    await repository.upsert_source(source)
-
-    return source
-
-
-@sources_router.get("/{uuid}")
-async def get_source(request: Request, uuid: str):
-    app = request.app
-    repository: Repository = app.state.repository
-    try:
-        source = await repository.get_source_by_id(uuid)
-    except RecordNotFoundError as e:
-        raise HTTPException(status_code=404, detail="Source not found") from e
-
-    return source
-
-
-@sources_router.delete("/{uuid}")
-async def delete_source(request: Request, uuid: str):
-    app = request.app
-    repository: Repository = app.state.repository
-    media_root = app.state.config.media_root
-
-    try:
-        await repository.get_source_by_id(uuid)
-    except RecordNotFoundError as e:
-        raise HTTPException(status_code=404, detail="Source not found") from e
-
-    # delete file from disk
-    path = os.path.join(media_root, "sources", str(uuid))
-    os.remove(path)
-
-    # delete source from database
-    await repository.delete_source_by_id(uuid)
